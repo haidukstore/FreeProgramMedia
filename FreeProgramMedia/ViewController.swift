5379dd57cadc11ea36f4173aae259c3d9dd58daa
haidukstore <hayduk.roman@gmail.com>
2020-09-05 13:19:56 +0300

    Initial Commit

---
+++ FreeProgramMedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreeProgramMedia
+//
+//  Created by Roman Haiduk on 05.09.2020.
+//  Copyright © 2020 Roman Haiduk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
